279fc89c1868cb3f602f7070bb21b97f86398b5f
Bekarys Magauiya <maagauiya@MacBook-Air-Bekarys.local>
2021-12-13 14:15:58 +0600

    7

--- app1/views.py
+++ app1/views.py
@@ -1,6 +1,11 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
-
+from .models import*
 # Create your views here.
 def index(request):
-    return HttpResponse("jdskdjks")
+   
+    items=Items.objects.all()
+    context={
+        "items" : items,
+    }
+    return render(request,'app1/index.html', context=context,)
